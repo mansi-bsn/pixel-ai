2908b4a3fcfb7a2deb1499fefbb435894d2d61b9
mansi <mansimaniya178@gmail.com>
2025-12-03 18:00:36 +0530

    Update Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 // import { dummyCreationData } from '../assets/assets'
 import { Sparkles } from 'lucide-react'
 import { Protect, useAuth } from '@clerk/clerk-react'
-import CreationItem from '../components/CreationItem'
+import CreationItem from '../components/CreationItem.jsx'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 
